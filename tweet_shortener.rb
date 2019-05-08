0958c915b7640b51fc1d994e7182d9ac311870fc
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-08 20:57:32 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,16 @@
 # Write your code here.
 
 def dictionary
-  
+  convert = {
+   "hello" => "hi",
+   "to" => "2",
+   "two" => "2",
+   "too" => "2",
+   "for" => "4",
+   "four" => "4",
+   "be" => "b",
+   "you" => "u",
+   "at" => "@",
+   "and" => "&"
+}
 end

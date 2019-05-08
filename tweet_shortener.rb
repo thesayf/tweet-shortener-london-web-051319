819e7bf3cd074ac36863f92c229a5fb224873e36
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-08 21:00:29 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,8 +16,6 @@ def dictionary
 end
 
 def word_substituter(tweet)
-  # replaces long words with their expected short form
-
     tweet.split(" ").map do |word|
       if dictionary.keys.include?(word.downcase)
         word = dictionary[word.downcase]
